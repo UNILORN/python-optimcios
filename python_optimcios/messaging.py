51cdfa20dbc85c91f2b9d1a3ab7f033b43f4711a
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2018-05-30 18:12:25 +0900

    Errorハンドリング追記とReconnect機能追加

--- python_optimcios/messaging.py
+++ python_optimcios/messaging.py
@@ -2,33 +2,43 @@ from websocket import create_connection
 
 
 class messaging:
-    def __init__(self, url, channel_id, access_token):
+    def __init__(self, url, channel_id, access_token, count=10):
         try:
             self.url = url
             self.channel = channel_id
             self.token = access_token
+            self.connectionCount = self.connectionCountDefine = count
         except Exception as err:
             print("[ ERROR ] Read Value Error")
+            print("[ ERROR LOG ] URL:" + url)
+            print("[ ERROR LOG ] Channel ID:" + channel_id)
+            print("[ ERROR LOG ] Access Token:" + access_token)
             print(err)
 
-    def connection(self) -> bool:
+    def connection(self, log=False) -> bool:
         try:
             ws_url = self.url + "?" + "channel_id=" + self.channel + "&access_token=" + self.token
+            if log:
+                print("[ LOG ] Connection URL:" + ws_url)
             self.ws = create_connection(ws_url)
             return True
         except Exception as err:
             print("[ ERROR ] CIOS Messaging Connection Error")
             print(err)
-            return False
+            res = self.__reconnection()
+            return res
 
-    def sendMessage(self, message) -> bool:
+    def sendMessage(self, message, log=False) -> bool:
         try:
+            if log:
+                print("[ LOG ] Send Message:" + message)
             self.ws.send(message)
             return True
         except Exception as err:
             print("[ ERROR ] CIOS Messaging Send Error")
             print(err)
             self.ws.close()
+            res = self.__reconnection()
             return False
 
     def receiveMessage(self) -> str:
@@ -40,3 +50,22 @@ class messaging:
             print(err)
             self.ws.close()
             return ""
+
+    def __reconnection(self, log=False) -> bool:
+        try:
+            ws_url = self.url + "?" + "channel_id=" + self.channel + "&access_token=" + self.token
+            if log:
+                print("[ LOG ] Connection URL:" + ws_url)
+            self.ws = create_connection(ws_url)
+            self.connectionCount = self.connectionCountDefine
+            return True
+        except Exception as err:
+            self.connectionCount -= 1
+            res = False
+            if self.connectionCount > 0:
+                res = self.__reconnection(log=log)
+            else:
+                print("[ ERROR ] Reconnect False")
+                exit()
+            return res
+
