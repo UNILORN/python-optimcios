99fd348e1842e30f8652ebf95793dc8fed2b56de
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2018-09-25 14:19:27 +0900

    close #6

--- python_optimcios/v2/authorization.py
+++ python_optimcios/v2/authorization.py
@@ -1,27 +1,15 @@
 import requests
-import urllib.parse
 import json
 
 
 class Authorization:
     def __init__(self, auth_uri="", client_id="", client_secret="", log=False):
-        """
-        :param auth_uri:
-        :param client_id: 
-        :param client_secret: 
-        :param log: 
-        """
         self.auth_url = "https://" + str(auth_uri) + "/connect/token"
         self.client_id = client_id
         self.client_secret = client_secret
         self.log = log
 
     def getRefreshAccessToken(self, scope="", refresh_token=""):
-        """
-        :param scope: 
-        :param refresh_token: 
-        :return: 
-        """
         self.__consoleLog("Run getRefreshAccessToken()")
 
         data = {
@@ -43,27 +31,16 @@ class Authorization:
         return res["access_token"]
 
     def __postAuthorizationData(self, url, data, headers):
-        """
-        :param url: 
-        :param data: 
-        :param headers: 
-        :return: 
-        """
         try:
             self.__consoleLog("HTTPRequest :method=POST :url=" + str(url))
             res = requests.post(url=url, data=data, headers=headers)
             self.__errorHandling(res.status_code)
         except:
-            self.__consoleLog("POST RequestError",True)
+            self.__consoleLog("POST RequestError", True)
             raise Exception("POST RequestError")
         return res.json()
 
     def __consoleLog(self, t, e=False) -> bool:
-        """
-        :param t: text 
-        :param e: error
-        :return: 
-        """
         if e:
             print("[ ERROR ] " + t)
         elif self.log:
@@ -73,11 +50,7 @@ class Authorization:
         return True
 
     def __errorHandling(self, status_code):
-        """
-        :param status_code: 
-        :return: 
-        """
         if not status_code == 200:
-            self.__consoleLog("StatusCode Error ["+ str(status_code)+"]", True)
+            self.__consoleLog("StatusCode Error [" + str(status_code) + "]", True)
             return Exception("StatusCode [" + str(status_code) + "] Error")
         return True
